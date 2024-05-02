61e99d21be931e6e756010ff4403a84074efc33e
MichalMiekina <michal.miekina98@gmail.com>
2024-05-02 20:11:38 +0200

    change a little structure of files and directories

--- src/main.js
+++ src/main.js
@@ -7,24 +7,11 @@ import Oferta from "./views/Oferta.vue";
 import Galeria from "./views/Galeria.vue";
 import Kontakt from "./views/Kontakt.vue";
 import Admin from "./views/Admin.vue";
+import VueRouter from "vue-router";
+import { firebaseConfig } from "./firebaseConfig";
 import { initializeApp } from 'firebase/app';
 
-import VueRouter from "vue-router";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC6cJfDfomCONwVTlUrZZop4C7HdYeoMXU",
-  authDomain: "md-detailing-dates.firebaseapp.com",
-  databaseURL: "https://md-detailing-dates-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "md-detailing-dates",
-  storageBucket: "md-detailing-dates.appspot.com",
-  messagingSenderId: "143908377299",
-  appId: "1:143908377299:web:0244dd277053854e277a26",
-  measurementId: "G-2DTYVQTDKH"
-};
-// Initialize Firebase
 initializeApp(firebaseConfig);
-// const db = getDatabase();
-
 
 Vue.use(VueRouter);
 const routes = [
@@ -43,11 +30,6 @@ const router = new VueRouter({
 });
 
 Vue.config.productionTip = false;
-
-// new Vue({
-//   router,
-//   render: h => h(App),
-// }).$mount('#app')
 
 new Vue({
   el: "#app",
